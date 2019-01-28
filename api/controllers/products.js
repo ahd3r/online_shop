8b375723548dc1d5923c5f9080f2d7b2cf2af738
ahd3r <ander11110@gmail.com>
2019-01-28 22:19:48 +0200

    Fix request but, but new bug with edit, patch request and with image. Dont end view (new requests)

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -14,43 +14,59 @@ class ProductController{
       res.send(data[0]);
     }).catch(err=>{
       console.log(err);
-      res.send({error:"Error in get data from mysql"});
+      res.send({error:"Error in get one data from mysql"});
     });
   }
   postProduct(req,res,next){
-    actionWithBD.addProduct(req.body);
-    actionWithBD.takeProducts().then(data=>{
-      res.send(data[0]);
+    actionWithBD.addProduct(req.body).then(data=>{
+      actionWithBD.takeProducts().then(data=>{
+        res.send(data[0]);
+      }).catch(err=>{
+        console.log(err);
+        res.send({error:"Error in get data from mysql"});
+      });
     }).catch(err=>{
       console.log(err);
-      res.send({error:'Error in get data from mysql'});
+      res.send({error:"Error in add data from mysql"});
     });
   }
   deleteProduct(req,res,next){
-    actionWithBD.deleteProduct(req.params.id);
-    actionWithBD.takeProducts().then(data=>{
-      res.send(data[0]);
+    actionWithBD.deleteProduct(parseInt(req.params.id)).then(data=>{
+      actionWithBD.takeProducts().then(data=>{
+        res.send(data[0]);
+      }).catch(err=>{
+        console.log(err);
+        res.send({error:"Error in get data from mysql"});
+      });
     }).catch(err=>{
       console.log(err);
-      res.send({error:'Error in get data from mysql'});
+      res.send({error:"Error in delete data from mysql"});
     });
   }
   editProduct(req,res,next){
-    actionWithBD.editProduct(req.body);
-    actionWithBD.takeProducts().then(data=>{
-      res.send(data[0]);
+    actionWithBD.editProduct(req.body).then(data=>{
+      actionWithBD.takeProducts().then(data=>{
+        res.send(data[0]);
+      }).catch(err=>{
+        console.log(err);
+        res.send({error:"Error in get data from mysql"});
+      });
     }).catch(err=>{
       console.log(err);
-      res.send({error:'Error in get data from mysql'});
+      res.send({error:"Error in edit data from mysql"});
     });
   }
   buyProduct(req,res,next){
-    actionWithBD.buyProduct(req.params.id);
-    actionWithBD.takeProducts().then(data=>{
-      res.send(data[0]);
+    actionWithBD.buyProduct(parseInt(req.params.id)).then(data=>{
+      actionWithBD.takeProducts().then(data=>{
+        res.send(data[0]);
+      }).catch(err=>{
+        console.log(err);
+        res.send({error:"Error in get data from mysql"});
+      });
     }).catch(err=>{
       console.log(err);
-      res.send({error:'Error in get data from mysql'});
+      res.send({error:"Error in (patch) data from mysql"});
     });
   }
 };
